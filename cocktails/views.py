bf318c263f2161a918d6c94a70752135c976b982
aobarbieri <aobarbieri@gmail.com>
2024-05-17 23:17:30 -0400

    Add Drink Details page and dummy data

--- cocktails/views.py
+++ cocktails/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from datetime import date
+
+from django.shortcuts import render
+
+drinks = [
+    {
+        "slug": "marguerita",
+        "image": "drink.jpg",
+        "author": "Amanda Mitzian",
+        "date": date(2021, 7, 21),
+        "title": "Original Marguerita",
+        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa unde asperiores laboriosam dolorem autem quos atque dolor, necessitatibus, saepe molestias ipsam culpa minus veritatis alias molestiae esse vel iure nam.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa unde asperiores laboriosam dolorem autem quos atque dolor, necessitatibus, saepe molestias ipsam culpa minus veritatis alias molestiae esse vel iure nam."
+    },
+    {
+        "slug": "marguerita",
+        "image": "drink.jpg",
+        "author": "Amanda Mitzian",
+        "date": date(2021, 7, 21),
+        "title": "Original Marguerita",
+        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa unde asperiores laboriosam dolorem autem quos atque dolor, necessitatibus, saepe molestias ipsam culpa minus veritatis alias molestiae esse vel iure nam.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa unde asperiores laboriosam dolorem autem quos atque dolor, necessitatibus, saepe molestias ipsam culpa minus veritatis alias molestiae esse vel iure nam."
+    }
+]
 
 # Create your views here.
 # Views are either a function or a class
@@ -13,5 +36,5 @@ def drinks(req):
     return render(req, 'cocktails/all-drinks.html')
 
 
-def drink_details(request):
-    pass
+def drink_details(req, slug):
+    return render(req, 'cocktails/drink-details.html')
