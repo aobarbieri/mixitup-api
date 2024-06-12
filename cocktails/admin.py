a19e13e342a79d0882801b1f15181bf5726b0335
aobarbieri <aobarbieri@gmail.com>
2024-06-12 15:59:14 -0400

    Register models into admin.py

--- cocktails/admin.py
+++ cocktails/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Strength, Flavor, Ingredient, Drink, DrinkIngredient, Instruction
 
 # Register your models here.
+class StrengthAdmin(admin.ModelAdmin):
+    pass
+
+
+class FlavorAdmin(admin.ModelAdmin):
+    pass
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+class DrinkAdmin(admin.ModelAdmin):
+    pass
+
+class DrinkIngredientAdmin(admin.ModelAdmin):
+    pass
+
+class InstructionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Strength, StrengthAdmin)
+admin.site.register(Flavor, FlavorAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Drink, DrinkAdmin)
+admin.site.register(DrinkIngredient, DrinkIngredientAdmin)
+admin.site.register(Instruction, InstructionAdmin)
