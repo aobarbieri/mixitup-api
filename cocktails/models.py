a229f7de9eb908816664eb0b72eada0fb4d0d595
aobarbieri <aobarbieri@gmail.com>
2024-06-12 15:32:44 -0400

    Update Models relationship and fields

--- cocktails/models.py
+++ cocktails/models.py
@@ -5,30 +5,36 @@ from django.core.validators import MinLengthValidator
 # Make migrations after modifying this file
 # python3 manage.py makemigrations
 # python3 manage.py migrate
-
-class Drink(models.Model):
-    name = models.CharField(max_length=20)
-    date = models.DateField(auto_now=True)
-    image_name = models.CharField(max_length=100, null=True)
-    slug = models.SlugField(unique=True, null=True)
-
-class Instruction(models.Model):
-    description = models.TextField(validators=[MinLengthValidator(10)])
-    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=30)
-    amount = models.DecimalField(max_digits=4, decimal_places=2)
-    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
-    
-class Strength(models.Model):
-    name = models.CharField(max_length=20)
-
-class Flavor(models.Model):
-    name = models.CharField(max_length=20)
-    
-    
 # To clear old records when migrating (only during development):
 # python3 manage.py shell
 # from projectFolderName.models import ModelName
 # ClassName.objects.all().delete()
+   
+class Strength(models.Model):
+    name = models.CharField(max_length=20)
+    
+class Flavor(models.Model):
+    name = models.CharField(max_length=20)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+
+class Drink(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, null=True)
+    
+    strength = models.ForeignKey(Strength, null=True, on_delete=models.SET_NULL)
+    flavors = models.ManyToManyField(Flavor, null=True)
+    ingredients = models.ManyToManyField(Ingredient, null=True, through='DrinkIngredient')
+
+class DrinkIngredient(models.Model):
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+class Instruction(models.Model):
+    description = models.TextField(validators=[MinLengthValidator(10)])
+    
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
